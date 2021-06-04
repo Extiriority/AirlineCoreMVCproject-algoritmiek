4781709535efcdc3778956c3602a7cc4f104e42e
Giang <463189@student.fontys.nl>
2021-06-05 00:41:41 +0200

    Add euler exercises

---
+++ Algoritme-euler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Algoritme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Euler algorithm practice
+
+            /*int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"Answer 1 = {sum}");*/
+
+            int PreviousNum = 1;
+            int currentNum = 2;
+            int sum = currentNum;
+            while (currentNum <= 4000000)
+            {
+                int tempNum = PreviousNum;
+                PreviousNum = currentNum;
+                currentNum = tempNum + currentNum;
+                if (currentNum % 2 == 0)
+                {
+                    sum += currentNum;
+                }
+            }
+            Console.WriteLine($"Answer 2 = {sum}");
+        }
+    }
+}
